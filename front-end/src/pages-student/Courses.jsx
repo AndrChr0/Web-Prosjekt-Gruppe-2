0befe17ab247dbb56f77dccaa5470d976f370dcf
MajaBru <122542734+MajaBru@users.noreply.github.com>
2024-03-03 20:13:52 +0100

    displaying all courses on courses page + some simple styles

--- front-end/src/pages-student/Courses.jsx
+++ front-end/src/pages-student/Courses.jsx
@@ -1,7 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 function Courses(){
+
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      setLoading(true);
+      axios
+        .get("http://localhost:5151/courses")
+        .then((res) => {
+          setCourses(res.data.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+        });
+    }, []);
+
     return(
-        <h1>My Courses</h1>
-    )
-    }
+        <div className="Courses-list">
+            <h1>My Courses</h1>
+            <ul>
+            {courses.map((course, i) => (
+                <li className="Course-item" key={course._id}>
+                    <div>{i + 1}</div>
+                    <div>
+                        <span><b>Course name:</b></span> {course.courseCode} {course.title} 
+                    </div>
+                    <div>
+                        <span><b>Course description:</b></span> {course.description} 
+                    </div>
+                </li>
+            ))}
+            </ul>
+        </div>
+    )}
     
-    export default Courses
+    export default Courses;
