0077e43cbd394069fe030daa817a3a1cdddab3a2
MajaBru <122542734+MajaBru@users.noreply.github.com>
2024-03-03 19:50:47 +0100

    added put and delete method

--- back-end/routes/courseRoute.js
+++ back-end/routes/courseRoute.js
@@ -34,10 +34,51 @@ router.post("/", async (req, res) => {
             res.status(201).send(savedCourse);
         }
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send(error);
+    }
+});
+
+// update a course
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        if (!req.body.title || !req.body.description || !req.body.courseCode) {
+            return res.status(400).send({
+                message: "Send all required fields: title, description, courseCode",
+            });
+        }
+
+        const result = await Course.findByIdAndUpdate(id, req.body);
+
+        if (!result) {
+            return res.status(404).json({ message: "Course not found" });
+        }
+
+        return res.status(200).send({ message: "Course updated successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
     }
 });
 
 
+
+// delete a course
+router.delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const result = await Course.findByIdAndDelete(id);
+  
+      if (!result) {
+        return res.status(404).json({ message: "course not found" });
+      }
+  
+      return res.status(200).send({ message: "course deleted successfully" });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send({ message: error.message });
+    }
+  });
 
 export default router;
