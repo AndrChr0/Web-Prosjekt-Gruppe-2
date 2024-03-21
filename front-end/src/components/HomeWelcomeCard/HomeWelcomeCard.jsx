4e8ab9146f4f018f2ab6df98574a212671b42467
Andreas Christiansen <andycchhrr@gmail.com>
2024-03-21 15:05:20 +0100

    fixed title + added favicon + created the welcome card base code

---
+++ front-end/src/components/HomeWelcomeCard/HomeWelcomeCard.jsx
@@ -0,0 +1,26 @@
+function HomeWelcomeCard() {
+  return (
+    <div className="card-container">
+      <h2 className="home-card-header">Welcome to the sustainability diary</h2>
+      <p className="home-card-text">
+        Welcome to the Sustainability Diary, your new space for growth and
+        reflection on sustainability! Created as part of NTNU's initiative to
+        integrate sustainability into education, this platform offers you a
+        unique opportunity to document, reflect, and share your sustainability
+        journey. Whether you're a student exploring your thoughts and projects,
+        or a teacher guiding the next generation, the Sustainability Diary is
+        designed to foster a deeper understanding of sustainability practices.
+        Track your progress, and become part of a community dedicated to making
+        a difference. Dive in and start your sustainability diary today, where
+        every entry is a step towards a more sustainable future.
+        <i>
+          Please note that you're exploring the beta version of our site, where
+          we're still fine-tuning features and not all functionalities are fully
+          implemented yet.
+        </i>
+      </p>
+    </div>
+  );
+}
+
+export default HomeWelcomeCard
