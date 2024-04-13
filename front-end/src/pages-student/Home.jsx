ce4efa2a7421b9e042a6281cccaa57398c6c7043
Andreas Christiansen <andycchhrr@gmail.com>
2024-04-13 18:27:13 +0200

    fixd bug in home jsx

--- front-end/src/pages-student/Home.jsx
+++ front-end/src/pages-student/Home.jsx
@@ -1,28 +1,7 @@
-import { useEffect, useState } from "react";
 import HomeWelcomeCard from "../components/HomeWelcomeCard/HomeWelcomeCard";
-import axios from "axios";
-import "../assets/styles/homeStyles.css";
 
-// import AddCourse from "./pages-student/AddCourse";
 
 function Home() {
-  // const [reflections, setReflections] = useState([]);
-  // const [loading, setLoading] = useState(false);
-
-  // useEffect(() => {
-  //   setLoading(true);
-  //   axios
-  //     .get("http://localhost:5151/reflections")
-  //     .then((res) => {
-  //       setReflections(res.data.data);
-  //       setLoading(false);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
   return (
     <main>
     <h1>Home | Student</h1>
