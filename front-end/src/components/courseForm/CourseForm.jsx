cdda3dd527be0ba0e99aea9e689b6f43e9429886
Andreas Christiansen <andycchhrr@gmail.com>
2024-04-16 17:01:27 +0200

    cleaned some code

--- front-end/src/components/courseForm/CourseForm.jsx
+++ front-end/src/components/courseForm/CourseForm.jsx
@@ -4,72 +4,72 @@ import axios from "axios";
 import "./courseForm.css";
 
 const CourseForm = () => {
- 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  const [courseData, setCourseData] = useState({
+    title: "",
+    courseCode: "",
+  });
 
-    const  [courseData, setCourseData] = useState({
+  const handleChange = (e) => {
+    setCourseData({ ...courseData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("authToken");
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5151/courses",
+        courseData,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // Include the JWT token
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Response:", response.data);
+      navigate("/my_courses");
+      setCourseData({
         title: "",
         courseCode: "",
-    });
-
-    const handleChange = (e) => {
-        setCourseData({ ...courseData, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const token = localStorage.getItem("authToken");
-        
-        try {
-            const response = await axios.post(
-                "http://localhost:5151/courses",
-                courseData,
-                {
-                    headers: {
-                      Authorization: `Bearer ${token}`, // Include the JWT token
-                      "Content-Type": "application/json",
-                    },
-                  }
-            );
-            console.log("Response:", response.data); 
-            navigate("/my_courses");
-            setCourseData({
-                title: "",
-                courseCode: "",
-            });
-        } catch (error) {
-            console.error(error);
-        }
+      });
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    return (
-        <div>
-            <form className="newCourse_form" onSubmit={handleSubmit}>
-                <label>
-                Course Name:
-                <input 
-                type="text" 
-                name="title" 
-                value={courseData.title}
-                onChange={handleChange}
-                required />
-                </label>
+  return (
+    <div>
+      <form className="newCourse_form" onSubmit={handleSubmit}>
+        <label>
+          Course Name:
+          <input
+            type="text"
+            name="title"
+            value={courseData.title}
+            onChange={handleChange}
+            required
+          />
+        </label>
 
-                <label>
-                Course Code:
-                <input 
-                type="text" 
-                name="courseCode" 
-                value={courseData.courseCode}
-                onChange={handleChange}
-                required />
-                </label>
-                
-                <input className="submit-btn" type="submit" value="Submit" />
-            </form>
-        </div>
-    );
-}
+        <label>
+          Course Code:
+          <input
+            type="text"
+            name="courseCode"
+            value={courseData.courseCode}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <input className="submit-btn" type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default CourseForm;
