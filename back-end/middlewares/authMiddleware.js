39c2776c2ff7c9b94354768aa2b8f8885923ecb9
Andreas Christiansen <andycchhrr@gmail.com>
2024-04-29 15:46:58 +0200

    again edgar*

--- back-end/middlewares/authMiddleware.js
+++ back-end/middlewares/authMiddleware.js
@@ -4,10 +4,8 @@ const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    console.log("Token received:", token); 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
-        console.log("Token verification error:", err); 
         return res.status(403).json({ message: "Token is not valid" });
       }
       req.user = decoded;
