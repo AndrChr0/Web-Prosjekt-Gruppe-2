6c085eda21644ce17bac8311ecfc0f0c5561f6be
Andreas Christiansen <andycchhrr@gmail.com>
2024-04-30 18:36:15 +0200

    cors debugging

--- back-end/index.js
+++ back-end/index.js
@@ -25,7 +25,25 @@ app.use(express.json());
 
 
 // Middelware for handeling CORS policy.
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Use "http://localhost:5174" for a specific origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+
+app.use((req, res, next) => {
+  console.log('Received headers:', req.headers);
+  next();
+});
+
 
 app.use("/reflections", verifyToken, reflectionRoute);
 app.use("/courses", verifyToken, courseRoute);
