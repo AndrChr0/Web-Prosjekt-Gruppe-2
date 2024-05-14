abaddc5d19cbfa0581872e5595426c79567f7c16
Andreas Christiansen <andycchhrr@gmail.com>
2024-05-14 13:23:24 +0200

    more minor

--- front-end/src/components/Register/Register.jsx
+++ front-end/src/components/Register/Register.jsx
@@ -5,8 +5,8 @@ import "./Register.css";
  
 const Register = () => {
 
-const apiURL = '/api';
-// const apiURL = import.meta.env.VITE_URL;
+// const apiURL = '/api';
+const apiURL = import.meta.env.VITE_URL;
 
 
   const [credentials, setCredentials] = useState({
