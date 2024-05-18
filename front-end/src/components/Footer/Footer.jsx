90cd8019fa042569485a013ef9e7a06a848c07a0
edgardahl <72726911+edgardahl@users.noreply.github.com>
2024-05-18 21:46:13 +0200

    small fix

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <footer>
-        <p className='footer-text'> Need any help? contact us at <a href="mailto:web-project.group2@ntnu.no">web-project.group2@ntnu.no</a> </p>
+        <p className='footer-text'> Need any help? Contact us at <a href="mailto:web-project.group2@ntnu.no">web-project.group2@ntnu.no</a> </p>
     </footer>
   );
 };
